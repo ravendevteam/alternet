3dca52c8ce44662d2dee8920c0dc326adfb40af7
snqre <marco.bizxaro@gmail.com>
2025-12-09 08:06:05 +0100

    add dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
+use libp2p::core::upgrade;
+use libp2p::identity;
+use libp2p::multiaddr;
+use libp2p::noise;
+use libp2p::quic;
+use libp2p::swarm;
+use libp2p::tcp;
+use libp2p::yamux;
+
+#[tokio::main]
+async fn main() {
     println!("Hello, world!");
 }
