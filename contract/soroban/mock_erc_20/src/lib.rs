c7f908b88e75973bc5681288c60c8c1964b1476e
snqre <marco.bizxaro@gmail.com>
2026-05-28 21:12:17 +0200

    add methods to mock erc20 implementation

--- contract/soroban/mock_erc_20/src/lib.rs
+++ contract/soroban/mock_erc_20/src/lib.rs
@@ -7,7 +7,11 @@ pub enum MemoryStoreKey {
 	Symbol,
 	Decimals,
 	TotalSupply,
-	Balance(soroban_sdk::Address)
+	Balance(soroban_sdk::Address),
+	Allowance(
+		soroban_sdk::Address, 
+		soroban_sdk::Address
+	)
 }
 
 #[soroban_sdk::contract]
@@ -84,6 +88,13 @@ impl Main {
 		state.get(&MemoryStoreKey::Balance(account)).unwrap_or(zero)
 	}
 	
+	pub fn allowance(environment: soroban_sdk::Env, account: soroban_sdk::Address, spender: soroban_sdk::Address) -> soroban_sdk::U256 {
+		let state: soroban_sdk::storage::Persistent = environment.storage().persistent();
+		let zero: soroban_sdk::U256 = soroban_sdk::U256::from_u32(&environment, 0);
+		
+		state.get(&MemoryStoreKey::Allowance(account, spender)).unwrap_or(zero)
+	}
+
 	pub fn mint(environment: soroban_sdk::Env, account: soroban_sdk::Address, amount: soroban_sdk::U256) {
 		let state: soroban_sdk::storage::Persistent = environment.storage().persistent();
 		let owner: soroban_sdk::Address = state.get(&MemoryStoreKey::Owner).expect("set on awakening");
@@ -102,11 +113,56 @@ impl Main {
 	}
 	
 	pub fn burn(environment: soroban_sdk::Env, account: soroban_sdk::Address, amount: soroban_sdk::U256) {
+		account.require_auth();
+		
 		let state: soroban_sdk::storage::Persistent = environment.storage().persistent();
 		let owner: soroban_sdk::Address = state.get(&MemoryStoreKey::Owner).expect("set on awakening");
 		
 		owner.require_auth();
 		
+		let balance: soroban_sdk::U256 = Self::balance_of(Clone::clone(&environment), Clone::clone(&account));
+		
+		if balance < amount {
+			panic!("insufficient balance")
+		}
+		
+		let balance: soroban_sdk::U256 = balance.sub(&amount);
+		
+		state.set(&MemoryStoreKey::Balance(account), &balance);
+		
+		let total_supply: soroban_sdk::U256 = Self::total_supply(Clone::clone(&environment));
+		let total_supply: soroban_sdk::U256 = total_supply.sub(&amount);
+		
+		state.set(&MemoryStoreKey::TotalSupply, &total_supply);
+	}
+	
+	pub fn approve(environment: soroban_sdk::Env, source: soroban_sdk::Address, spender: soroban_sdk::Address, amount: soroban_sdk::U256) {
+		source.require_auth();
+		
+		let state: soroban_sdk::storage::Persistent = environment.storage().persistent();
+		
+		state.set(&MemoryStoreKey::Allowance(source, spender), &amount);
+	}
+	
+	pub fn transfer(environment: soroban_sdk::Env, sender: soroban_sdk::Address, recipient: soroban_sdk::Address, amount: soroban_sdk::U256) {
+		sender.require_auth();
+		
+		let state: soroban_sdk::storage::Persistent = environment.storage().persistent();
+		let sender_balance: soroban_sdk::U256 = Self::balance_of(Clone::clone(&environment), Clone::clone(&sender));
+	
+		if sender_balance < amount {
+			panic!("insufficient balance")
+		}
+		
+		let sender_balance = sender_balance.sub(&amount);
+		let recipient_balance: soroban_sdk::U256 = Self::balance_of(Clone::clone(&environment), Clone::clone(&recipient));
+		let recipient_balance: soroban_sdk::U256 = recipient_balance.add(&amount);
+	
+		state.set(&MemoryStoreKey::Balance(sender), &sender_balance);
+		state.set(&MemoryStoreKey::Balance(recipient), &recipient_balance);
+	}
+	
+	pub fn transfer_from(environment: soroban_sdk::Env) {
 		
 	}
 }
