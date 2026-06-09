c1a81c51788fcb241730e3193586f089f6e20d53
snqre <marco.bizxaro@gmail.com>
2026-06-09 22:45:24 +0200

    update stream abstraction

--- app/node/src/sub_system/session_manager.rs
+++ app/node/src/sub_system/session_manager.rs
@@ -9,45 +9,64 @@ use tokio_util::compat::FuturesAsyncReadCompatExt as _;
 
 pub const PROTOCOL: libp2p::StreamProtocol = libp2p::StreamProtocol::new("/an/0.1.0");
 
-pub struct ConnectionRequest {
-	dst: libp2p::PeerId
+#[derive(Debug)]
+#[derive(Clone)]
+#[derive(PartialEq)]
+#[derive(Eq)]
+pub struct Connection {
+	pub peer: libp2p::PeerId
 }
 
-pub struct ForwardRequest {
-	src: libp2p::PeerId,
-	dst: libp2p::PeerId
+#[derive(Debug)]
+pub struct ConnectionEstablishment {
+	pub src: libp2p::PeerId,
+	pub dst: libp2p::PeerId,
+	pub stream: libp2p::Stream
 }
 
+#[derive(Debug)]
+#[derive(Clone)]
+#[derive(PartialEq)]
+#[derive(Eq)]
+pub struct Disconnection {
+	pub peer: libp2p::PeerId
+}
+
+#[derive(Debug)]
+#[derive(Clone)]
+#[derive(PartialEq)]
+#[derive(Eq)]
 pub struct BytesInbound {
-	src: libp2p::PeerId,
-	content: bytes::BytesMut
+	pub src: libp2p::PeerId,
+	pub content: bytes::Bytes
 }
 
+#[derive(Debug)]
+#[derive(Clone)]
+#[derive(PartialEq)]
+#[derive(Eq)]
 pub struct BytesOutbound {
-	dst: libp2p::PeerId,
-	content: bytes::BytesMut
-}
-
-pub struct Send<T> {
-	
-}
-
-pub enum Signal {
-	Session {
-		session_key: String,
-		peer: libp2p::PeerId
-	},
-	Ready {
-		src: libp2p::PeerId,
-		dst: libp2p::PeerId,
-		stream: libp2p::Stream
-	}
+	pub dst: libp2p::PeerId,
+	pub content: bytes::Bytes
 }
 
 pub struct SessionManager {
-	sx: tokio::sync::mpsc::Sender<Signal>,
-	rx: tokio::sync::mpsc::Receiver<Signal>,
-	session_key_to_peer_id: std::collections::HashMap<u64, libp2p::PeerId>
+	peer_to_bytes_sx: std::collections::HashMap<libp2p::PeerId, tokio::sync::mpsc::Sender<bytes::Bytes>>,
+	event_sx: tokio::sync::mpsc::Sender<Event>,
+	event_rx: tokio::sync::mpsc::Receiver<Event>,
+	boot: bool
+}
+
+impl Default for SessionManager {
+	fn default() -> Self {
+    	let (event_sx, event_rx) = tokio::sync::mpsc::channel(1000);
+		Self {
+			peer_to_bytes_sx: std::collections::HashMap::default(),
+			event_sx,
+			event_rx,
+			boot: false
+     	}
+	}
 }
 
 impl SubSystem for SessionManager {
@@ -56,66 +75,133 @@ impl SubSystem for SessionManager {
 		swarm: &mut Swarm, 
 		event: &mut Event, 
 		queue: &mut dyn FnMut(Event)
-	) {
-		if let Some(ConnectionRequest) = event.downcast_ref() {
-			let mut control = swarm.behaviour_mut().stream.new_control().to_owned();
+	) {	
+		while let Ok(event) = self.event_rx.try_recv() {
+			if let Some(Disconnection {
+				peer
+			}) = event.downcast_ref() {
+				self.peer_to_bytes_sx.remove(peer);
+			}
+			
+			queue(event);
+		}
+		
+		if !self.boot {
+			self.boot = true;
+			
+			let mut control: libp2p_stream::Control = swarm.behaviour_mut().stream.new_control().to_owned();
+			let Ok(mut streams) = control.accept(PROTOCOL) else {
+				panic!("session manager subsystem failed to accept protocol")
+			};
+			let dst: libp2p::PeerId = swarm.local_peer_id().to_owned();
+			let event_sx: tokio::sync::mpsc::Sender<_> = self.event_sx.to_owned();
 			
 			tokio::spawn(async move {
-				match control.open_stream("", PROTOCOL).await {
+				while let Some((peer, stream)) = streams.next().await {
+					let src: libp2p::PeerId = peer;
+					let event: Event = Event::new(ConnectionEstablishment {
+						src,
+						dst,
+						stream
+					});
 					
+					if event_sx.send(event).await.is_err() {
+					    break
+				    }
 				}
 			});
 		}
 		
-    	while let Ok(signal) = self.rx.try_recv() {
-     		match signal {
-       			Signal::Session {
-       				session_key,
-          			peer
-        		} => {
-          			tokio::spawn(async move {
-             			
-             		});
-          		},
-           		Signal::Ready {
-             		src,
-               		dst,
-             		stream
-            	} => {
-             		let stream: tokio_util::compat::Compat<_> = stream.compat();
-             		let stream: tokio_util::codec::Framed<_> = tokio_util::codec::Framed::new(stream, tokio_util::codec::LengthDelimitedCodec::default());
-             		let (mut stream_sx, mut stream_rx) = stream.split();
-               		let (src_sx, src_rx) = tokio::sync::mpsc::channel(1000);
-                 	let (dst_sx, dst_rx) = tokio::sync::mpsc::channel(1000);
-                  	let mut buffer: Vec<_> = vec![0; 4000];
-                   
-                   	// src_rx => receive bytes in
-                    // dst_sx => send bytes out
-                
-             		tokio::spawn(async move {
-               			while let Some(s) = stream_rx.next().await {
-                  			match s {
-                     			Ok(bytes) => {
-                        			if src_sx.send(bytes).await.is_err() {
-                           				break
-                           			}
-                        		},
-                          		Err(_) => {
-                            		break
-                            	}
-                     		}
-                  		}
-               		});
-               
-               		tokio::spawn(async move {
-                 		while let Some(bytes) = dst_rx.recv().await {
-                   			if stream_sx.send(bytes).await.is_err() {
-                      			break
-                      		}
-                   		}
-                 	});
-             	}
-       		}
-     	}
+		if let Some(Connection {
+			peer
+		}) = event.downcast_ref() {
+			let mut control: libp2p_stream::Control = swarm.behaviour_mut().stream.new_control().to_owned();
+			let src: libp2p::PeerId = swarm.local_peer_id().to_owned();
+			let dst: libp2p::PeerId = peer.to_owned();
+			let event_sx: tokio::sync::mpsc::Sender<_> = self.event_sx.to_owned();
+			
+			tokio::spawn(async move {
+				let Ok(stream) = control.open_stream(dst.to_owned(), PROTOCOL).await else {
+					return
+				};
+				
+				let event: Event = Event::new(ConnectionEstablishment {
+					src,
+					dst,
+					stream
+				});
+				
+				event_sx.send(event).await.ok();
+			});
+		}
+		
+		if let Some(BytesOutbound {
+			dst,
+			content
+		}) = event.downcast_ref() 
+		&& let Some(session_sx) = self.peer_to_bytes_sx.get(dst) {
+			let session_sx: tokio::sync::mpsc::Sender<_> = session_sx.to_owned();
+			let content: bytes::Bytes = content.to_owned();
+			
+			tokio::spawn(async move {
+				session_sx.send(content).await.ok();
+			});
+		}
+		
+		if let Some(ConnectionEstablishment {
+			src,
+			dst,
+			stream
+		}) = event.downcast_ref() {
+			let src: libp2p::PeerId = src.to_owned();
+			let dst: libp2p::PeerId = dst.to_owned();
+			let remote_peer: libp2p::PeerId = if src == swarm.local_peer_id().to_owned() {
+				dst
+			} else {
+				src
+			};
+			let event_sx: tokio::sync::mpsc::Sender<_> = self.event_sx.to_owned();
+			let stream: tokio_util::compat::Compat<_> = stream.compat();
+			let stream: tokio_util::codec::Framed<_, _> = tokio_util::codec::Framed::new(stream, tokio_util::codec::LengthDelimitedCodec::default());
+			let (mut stream_sx, mut stream_rx) = stream.split();
+			// let (src_sx, src_rx) = tokio::sync::mpsc::channel(1000);
+			let (dst_sx, dst_rx) = tokio::sync::mpsc::channel(1000);
+			let mut buffer: Vec<_> = vec![0; 4000];
+			
+			self.peer_to_bytes_sx.insert(remote_peer, dst_sx);
+
+			tokio::spawn(async move {
+				while let Some(Ok(bytes)) = stream_rx.next().await {
+					let content: bytes::Bytes = bytes.freeze();
+					let event: Event = Event::new(BytesInbound {
+						src: remote_peer.to_owned(),
+						content
+					});
+					if event_sx.send(event).await.is_err() {
+						break
+					}
+				}
+				
+				let event: Event = Event::new(Disconnection {
+					peer: remote_peer
+				});
+				
+				event_sx.send(event).await.ok();
+			});
+			
+			tokio::spawn(async move {
+				while let Some(bytes) = dst_rx.recv().await {
+					if stream_sx.send(bytes).await.is_err() {
+						break
+					}
+				}
+			});
+		}
+		
+		if let Some(Disconnection {
+			peer
+		}) = event.downcast_ref() {
+			self.peer_to_bytes_sx.remove(peer);
+		}
 	}
 }
