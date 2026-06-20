bb28a0bf0e41b7ab0d4b4fb3c8d92984f9fd7b2f
snqre <marco.bizxaro@gmail.com>
2026-06-20 17:01:51 +0200

    update packet

--- lib/packet/src/opcode.rs
+++ lib/packet/src/opcode.rs
@@ -5,14 +5,29 @@ pub struct IsFromMarkedSignedUnverified;
 pub struct IsFromSignedVerified;
 pub struct IsFromSignedUnverified;
 pub struct IsFromUnsigned;
+pub struct IsUnavailable;
 
 #[derive(Debug)]
 #[derive(Clone)]
-pub struct Call<A, B, C = IsUnknownProtocol, D = IsFromUnsigned, E = (), F = ()> {
+pub struct Call<A, B, C = IsUnknownProtocol, D = IsFromUnsigned, E = IsUnavailable, F = IsUnavailable> {
 	phantom_data: std::marker::PhantomData<(B, C, D)>,
 	pkt: A,
 	signer: E,
 	signature: F
+}
+
+impl<A, B, C> Call<A, B, C, IsFromMarkedSignedVerified, lib_cryptography::public_key::PublicKey<B>, lib_cryptography::signature::Signature<B>> {
+	pub fn pkt(&self) -> &A {
+		&self.pkt
+	}
+	
+	pub fn signer(&self) -> &lib_cryptography::public_key::PublicKey<B> {
+		&self.signer
+	}
+	
+	pub fn signature(&self) -> &lib_cryptography::signature::Signature<B> {
+		&self.signature
+	}
 }
 
 impl<A, B, C> TryFrom<lib_bytes::NonEmpty> for Call<A, B, C, IsFromMarkedSignedVerified, lib_cryptography::public_key::PublicKey<B>, lib_cryptography::signature::Signature<B>> 
@@ -25,10 +40,28 @@ where
 	fn try_from(value: lib_bytes::NonEmpty) -> Result<Self, Self::Error> {
 		let out: MarkedSignedUnverified<B, C> = value.try_into()?;
 		let out: MarkedSignedVerified<_, _> = out.try_into()?;
-		let (signature, signer, message) = out.into();
+		let (signature, signer, pkt) = out.into();
 		Ok(Self {
 			phantom_data: std::marker::PhantomData,
-			pkt: message.try_into()?,
+			pkt: pkt.try_into()?,
+			signer,
+			signature
+		})
+	}
+}
+
+impl<A, B, C> TryFrom<lib_bytes::NonEmpty> for Call<A, B, C, IsFromMarkedSignedUnverified, lib_cryptography::public_key::PublicKey<B>, lib_cryptography::signature::Signature<B>>
+where
+	A: TryFrom<Unsigned<C>, Error = Box<dyn std::error::Error>>,
+	B: lib_cryptography::AsymmetricSetLayout {
+	type Error = Box<dyn std::error::Error>;
+	
+	fn try_from(value: lib_bytes::NonEmpty) -> Result<Self, Self::Error> {
+		let out: MarkedSignedUnverified<B, C> = value.try_into()?;
+		let (signature, signer, pkt) = out.into();
+		Ok(Self {
+			phantom_data: std::marker::PhantomData,
+			pkt: pkt.try_into()?,
 			signer,
 			signature
 		})
